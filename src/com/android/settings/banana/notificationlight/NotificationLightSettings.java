LMODroid/platform_packages_apps_Settings

NotificationLight: Add missing Dialog Metrics
Change-Id: If40b2b1b385d66b0ad37ae3777104210473d17ac

src/com/android/settings/libremobileos/notificationlight/NotificationLightSettings.java
@@ -550,6 +550,14 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         return dialog;
     }
 
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        if (dialogId == DIALOG_APPS) {
+            return MetricsEvent.LMODROID;
+        }
+        return 0;
+    }
+
     /**
      * Application class
      */

bananadroid/android_packages_apps_Settings

src/com/android/settings/banana/notificationlight/NotificationLightSettings.java
@@ -550,6 +550,14 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         return dialog;
     }
 
+    @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        if (dialogId == DIALOG_APPS) {
+            return MetricsEvent.BANANADROID;
+        }
+        return 0;
+    }
+
     /**
      * Application class
      */