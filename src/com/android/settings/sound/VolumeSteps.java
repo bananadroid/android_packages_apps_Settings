Evolution-X-AOSP/packages_apps_Settings

Settings: Allow customizing volume stream steps [2/3]
only media, calls and alarms seem fit
the rest are pretty much set and forget and don't need vol button granularity

Change-Id: I6ce6fa79df7b94ae84912945f6129048895067b2

src/com/android/settings/sound/VolumeSteps.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2024 Yet Another AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.sound;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.evolution.settings.preference.CustomSeekBarPreference;
+
+/**
+ * volume steps settings under sound
+ */
+@SearchIndexable
+public class VolumeSteps extends DashboardFragment implements OnPreferenceChangeListener {
+
+    private static final String TAG = "VolumeSteps";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.volume_steps_settings;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference pref = screen.getPreference(i);
+            if (!(pref instanceof CustomSeekBarPreference))
+                continue;
+            String key = pref.getKey();
+            final int def = Settings.System.getInt(resolver, "default_" + key, 15);
+            final int value = Settings.System.getInt(resolver, key, def);
+            CustomSeekBarPreference sbPref = (CustomSeekBarPreference) pref;
+            sbPref.setDefaultValue(def);
+            sbPref.setValue(value);
+            sbPref.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!(preference instanceof CustomSeekBarPreference))
+            return false;
+        Settings.System.putInt(getActivity().getContentResolver(),
+                preference.getKey(), (Integer) newValue);
+        return true;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVO_SETTINGS;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.volume_steps_settings);
+}

bananadroid/android_packages_apps_Settings

src/com/android/settings/sound/VolumeSteps.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2024 Yet Another AOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.sound;
+
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.banana.support.preferences.CustomSeekBarPreference;
+
+/**
+ * volume steps settings under sound
+ */
+@SearchIndexable
+public class VolumeSteps extends DashboardFragment implements OnPreferenceChangeListener {
+
+    private static final String TAG = "VolumeSteps";
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.volume_steps_settings;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen screen = getPreferenceScreen();
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            Preference pref = screen.getPreference(i);
+            if (!(pref instanceof CustomSeekBarPreference))
+                continue;
+            String key = pref.getKey();
+            final int def = Settings.System.getInt(resolver, "default_" + key, 15);
+            final int value = Settings.System.getInt(resolver, key, def);
+            CustomSeekBarPreference sbPref = (CustomSeekBarPreference) pref;
+            sbPref.setDefaultValue(def);
+            sbPref.setValue(value);
+            sbPref.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (!(preference instanceof CustomSeekBarPreference))
+            return false;
+        Settings.System.putInt(getActivity().getContentResolver(),
+                preference.getKey(), (Integer) newValue);
+        return true;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.BANANADROID;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.volume_steps_settings);
+}